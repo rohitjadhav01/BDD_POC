9d8b9ae1cd15c9d3c37bc19471be94d5452f35c2
rohitjadhav01 <31251216+rohitjadhav01@users.noreply.github.com>
2017-08-22 22:14:07 +0530

    Create Rectangle.cs

---
+++ Rectangle.cs
@@ -0,0 +1,21 @@
+using System;
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    } 
